310982c66c686410e3a46ca772d9925425850a20
Koooowal <jan.kowalski4b@o2.pl>
2025-04-02 12:38:44 +0200

    Update db.js

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'blog',
-  port: 3308
+  host: process.env.DB_HOST ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DB,
+  port: process.env.DB_PORT
 });
 
 
